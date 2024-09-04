0979019d21fcf93adda4e3473f2e96eaf7a2d5c0
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:37:20 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "SS",
-    heroTagline: "Sunday Stephens Realty",
-    agentName: "Sunday Stephens",
+    headerInitials: "LH",
+    heroTagline: "Lawson Hopper Realty",
+    agentName: "Lawson Hopper",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Sunday Stephens was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Sunday Stephens provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Sunday Stephens were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Sunday Stephens and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Sunday Stephens is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Lawson Hopper was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Lawson Hopper provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Lawson Hopper were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Lawson Hopper and His team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Lawson Hopper is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "",
-    address: "240 Applegate Pkwy \nPelham, AL 35124",
-    phone: "205.914.9460",
-    footerDescription: `Sunday Stephens Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "lawsonhopper@kw.com",
+    address: "3595 Grandview Pkwy, Suite 250 \nBirmingham, AL 35243",
+    phone: "205-901-5993",
+    footerDescription: `Lawson Hopper Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/sunday.stephens.98/",
+        facebook: "https://www.facebook.com/LawsonHopperRealtor",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
-        phone: "205.914.9460",
+        email: "lawsonhopper@kw.com",
+        phone: "205-901-5993",
     },
     website: "",
-    dp: "https://content.mediastg.net/dynamic/RealEstate/company/646/account/593902/593902_03092022182421.jpg" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/200589948_325577792275329_9038966963132641366_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=J0uRO35ZTyUQ7kNvgHwpdqL&_nc_ht=scontent-sjc3-1.xx&oh=00_AYAyAoEqVyrKpCtYXXf8lKhgQkiorssvTyO2r8ycSEdvrQ&oe=66FD5BDA" // Agent image URL
 };
 
 export default clientData;
